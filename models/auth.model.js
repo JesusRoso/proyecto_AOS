65257100a8597128a526ade5207e7b21c4005a22
Jesus Roso <jr8campeones@gmail.com>
2023-11-25 17:54:02 -0500

    fix:change url bd

--- models/auth.model.js
+++ models/auth.model.js
@@ -1,1 +1,11 @@
-import pgService from "../services/pg.service";
+import pgService from "../services/pg.service.js";
+
+export const getUserModel = async(usuario, clave) => {
+    const pg =  new pgService(); 
+    try{
+        return await pg.connection.oneOrNone(`SELECT id, usuario, clave FROM usuarios WHERE usuario = $1 AND clave = $2`,[usuario, clave]);
+    }
+    catch(error){
+        throw error;
+    }
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login } from "../controllers/auth.controller.js";
+import { getUser, login } from "../controllers/auth.controller.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { heroeValidator } from "../validators/heroe.validator.js";
+import { usuarioValidator } from "../validators/usuario.validator.js";
 const router = Router();
 
-router.get("/", login ) 
+//router.get("/", login ) 
+router.get("/",validate(usuarioValidator),getUser)
 
 export default router;
